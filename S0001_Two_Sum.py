485242d9a732886a28ed0de1d9b5741bf0e6381d
huang <tx.huang@haplox.com>
2021-08-05 17:40:36 +0800

    S0001_Two_Sum.py

---
+++ S0001_Two_Sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+  def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    d = {}
+    for i, num in enumerate(nums):
+      if target - num in d:
+        return [d[target - num], i]
+      d[num] = i
+    # no special case handling because it's assumed that it has only one solution
+
+def main():
+    test1 = Solution()
+    result = test1.twoSum([100,0,1,2,3,4], 103)
+    print(result)
+
+if __name__ == '__main__':
+    main()
